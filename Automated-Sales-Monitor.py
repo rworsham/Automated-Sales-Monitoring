a6fbac13dc5bd0d3133706edca2ac67e0351c4d5
rworsham <148657718+rworsham@users.noreply.github.com>
2023-12-11 17:15:18 -0500

    Add files via upload

--- Automated-Sales-Monitor.py
+++ Automated-Sales-Monitor.py
@@ -14,9 +14,9 @@ class Send:
         msg = EmailMessage()
         msg.set_content(message)
 
-        msg['Subject'] = 'Gastonia & Mt.Holly Sales'
+        msg['Subject'] = 'Catawba Sales'
         msg['From'] = "Dalcom Sales Monitor"
-        msg['To'] = "helpdesk@dalcom.com"
+        msg['To'] = "jwright@dalcom.com"
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
@@ -39,22 +39,30 @@ class Check:
 while True:
     current_time = time.strftime("%H:%M")
     check = Check()
-    check.grab("../INVMH/IN/STORE001.TXT")
-    check.grab("../INV/IN/1/STORE001.TXT")
-    check.grab("../INV/IN/2/STORE002.TXT")
-    check.grab("../INV/IN/3/STORE003.TXT")
-    check.grab("../INV/IN/4/STORE004.TXT")
-    check.grab("../INV/IN/6/STORE006.TXT")
+    check.grab("C:\\INV\\IN\\1\\STORE001.TXT")
+    check.grab("C:\\INV\\IN\\2\\STORE002.TXT")
+    check.grab("C:\\INV\\IN\\3\\STORE003.TXT")
+    check.grab("C:\\INV\\IN\\4\\STORE004.TXT")
+    check.grab("C:\\INV\\IN\\5\\STORE005.TXT")
+    check.grab("C:\\INV\\IN\\6\\STORE006.TXT")
+    check.grab("C:\\INV\\IN\\7\\STORE007.TXT")
+    check.grab("C:\\INV\\IN\\8\\STORE008.TXT")
+    check.grab("C:\\INV\\IN\\9\\STORE009.TXT")
+    check.grab("C:\\INV\\IN\\10\\STORE010.TXT")
+    check.grab("C:\\INV\\IN\\11\\STORE011.TXT")
+    check.grab("C:\\INV\\IN\\12\\STORE012.TXT")
+
+
     print("Polling Folders Checked")
     time.sleep(5)
-    if current_time == "22:00":
+    if current_time == "22:45" or len(polled_sales) == 12:
         send = Send()
         polled_sales_data = ("\n".join(polled_sales))
         number_of_sales = len(polled_sales)
         message = f"""
         Below is the list of Stores that have polled, compare the list below to the spreadsheet and proceed accordingly. 
-
-        Results Returned: {number_of_sales}                 Expected: 6
+        
+        Results Returned: {number_of_sales}                 Expected: 12
 
 {polled_sales_data}
         
@@ -63,7 +71,3 @@ while True:
         send.email(message)
         time.sleep(60)
         exit()
-
-
-
-
